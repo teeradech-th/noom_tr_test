245464c10e8b87de1830ef5989e87f85f36e3885
Teeradech-TH <t.thanapop@gmail.com>
2020-01-30 16:07:05 +0700

    fix port conflit MacOS update

--- api/src/index.js
+++ api/src/index.js
@@ -2,21 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import router from './routes';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 
 
 const app = express();
-app.use(cors);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(router);
-
-console.log('hi out route');
-app.get('/test', (req, res) => {
-  console.log('hi in route');
-  return res.sendStatus(200);
-})
 
 app.listen(process.env.PORT);
 console.log(`SERVER STARTED ON PORT ${process.env.PORT}`);
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,9 +2,6 @@ import { Router } from 'express';
 import instagram from './instagram';
 const router = Router();
 
-
-console.log('hi on import');
-
 router.use('/instagram', instagram);
 
 export default router;
